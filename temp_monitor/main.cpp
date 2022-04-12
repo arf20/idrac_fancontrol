7760cadfc2fb52f347832a2d1ba3ae5af588be41
arf20 <aruizfernandez05@gmail.com>
2022-04-12 18:44:54 +0200

    Fuck OpenIPMI, refactor, call ipmitool instead

--- temp_monitor/main.cpp
+++ temp_monitor/main.cpp
@@ -1,83 +1,88 @@
 #include <iostream>
-#include <cstring>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <thread>
 
-void die(const std::string& msg) {
-	std::cout << msg << std::endl;
-	exit(1);
+// Execute command, wait for it to finish and return its stdout
+std::string exec(const std::string& cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
 }
 
-extern "C" {
-	#include <OpenIPMI/ipmiif.h>
-	#include <OpenIPMI/ipmi_smi.h>
-	#include <OpenIPMI/ipmi_err.h>
-	#include <OpenIPMI/ipmi_auth.h>
-	#include <OpenIPMI/ipmi_lan.h>
-	#include <OpenIPMI/ipmi_posix.h>
-	#include <OpenIPMI/ipmi_fru.h>
-	#include <OpenIPMI/internal/ipmi_sensor.h>
-}
+// Variables
+float inletTemp = 0.0f;
+float exhaustTemp = 0.0f;
+float cpuTemps[2] = { 0.0f, 0.0f};
 
-#include <iostream>
+float fanSpeeds[6] = { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f };
 
-static os_handler_t *os_hnd;
+float totalPower = 0.0f;
 
-// On new (change) entity, search all sensors of it
-static void entity_change_handler(enum ipmi_update_e op, ipmi_domain_t *domain, ipmi_entity_t *entity, void *cb_data) {
-	int rv;
-	
-	int id = ipmi_entity_get_entity_id(entity);
-	int instance = ipmi_entity_get_entity_instance(entity);
-	
-	if (op == IPMI_ADDED) {
-		std::cout << "Entity added: " << id << "." << instance << " " << ipmi_entity_get_entity_id_string(entity) << std::endl;
+// Sensors
+const std::string inletTempSens = 	"Inlet Temp       ";
+const std::string exhaustTempSens = "Exhaust Temp     ";
+const std::string cpuTempSens = 	"Temp             ";
+
+const std::vector<std::string> fanSpeedSens = { "Fan1             ", "Fan2             ", "Fan3             ", "Fan4             ", "Fan5             ", "Fan6             "};
+
+const std::string totalPowerSens = 	"Pwr Consumption  ";
+
+// IPMI tool command
+const std::string ipmiCommand = "ipmitool sdr elist full";
+
+// Watch
+int watchloop() {
+	while (true) {
+		std::string ipmiout = exec(ipmiCommand);
+		std::istringstream ss(ipmiout);
+		std::string line;
+		int cpuTempIdx = 0;
+		while (std::getline(ss, line)) {
+			const std::string sens = line.substr(0, 17);
+			const std::string val = line.substr(38, std::string::npos);
+			
+			//std::cout << sens << ": " << val << ";" << std::endl; 
+			
+			if (sens == inletTempSens) inletTemp = (float)std::stoi(val);
+			if (sens == exhaustTempSens) exhaustTemp = (float)std::stoi(val);
+			if (sens == cpuTempSens) {
+				cpuTemps[cpuTempIdx] = (float)std::stoi(val);
+				cpuTempIdx++;
+			}
+			for (int i = 0; i < fanSpeedSens.size(); i++) {
+				if (sens == fanSpeedSens[i]) fanSpeeds[i] = (float)std::stoi(val);
+			}
+			if (sens == totalPowerSens) totalPower = (float)std::stoi(val);
+		}
+		
+		std::cout << "Temps:" << std::endl 
+			<< "\tInlet: " << inletTemp << "\tExhaust: " << exhaustTemp << std::endl
+			<< "\tCpu0: " << cpuTemps[0] << "\tCpu1: " << cpuTemps[1] << std::endl
+		<< "Fan Speeds" << std::endl
+			<< "\tFan1: " << fanSpeeds[0] << "\tFan2: " << fanSpeeds[1] << "\tFan3: " << fanSpeeds[2] << std::endl
+			<< "\tFan4: " << fanSpeeds[3] << "\tFan5: " << fanSpeeds[4] << "\tFan6: " << fanSpeeds[5] << std::endl
+		<< "Total Power Consumption" << std::endl
+			<< "\tPower: " << totalPower << std::endl;
 	}
 }
 
-// On connection done, search all entities in the system
-void setup_done_handler(ipmi_domain_t *domain, int err, unsigned int conn_num, unsigned int port_num, int still_connected, void *user_data) {
-	std::cout << "Setup done" << std::endl;
-	
-	int rv;
-	
-	if (rv = ipmi_domain_add_entity_update_handler(domain, entity_change_handler, domain))
-		die(std::string("Error in ipmi_domain_add_entity_update_handler: ") + strerror(rv));
-}
 
 int main(int argc, char **argv) {
-	// Allocate OS handler
-	os_hnd = ipmi_posix_setup_os_handler();
-	if (!os_hnd) die("Cound not allocate OS handler.");
+	std::thread watch(watchloop);
+	watch.detach();
 	
-	// Initialize OpenIPMI two times to detect bugs
-	int rv;
-	if (rv = ipmi_init(os_hnd))
-		die(std::string("Error in OpenIPMI initialization: ") + strerror(rv));
-		
-	ipmi_shutdown();
-	
-	if (rv = ipmi_init(os_hnd))
-		die(std::string("Error in OpenIPMI initialization (2): ") + strerror(rv));
-		
-	// Parse command arguments (connection stuff)
-	int curr_arg = 1;
-	ipmi_args_t *args;
-	if (rv = ipmi_parse_args2(&curr_arg, argc, argv, &args))
-		die(std::string("Error parsing command argument ") + std::to_string(curr_arg) + strerror(rv));
-		
-	// Setup arguments connection
-	ipmi_con_t *con;
-	if (rv = ipmi_args_setup_con(args, os_hnd, NULL, &con))
-		die(std::string("Error in ipmi_args_setup_con: ") + strerror(rv));
-		
-	// Open domain
-	std::cout << "Opening domain..." << std::endl;
-	if (rv = ipmi_open_domain("", &con, 1, setup_done_handler, NULL, NULL, NULL, NULL, 0, NULL))
-		die(std::string("Error in ipmi_open_domain: ") + strerror(rv));
-		
-	// Main loop of events
-	while (true)
-		os_hnd->perform_one_op(os_hnd, NULL);
-		
-	// Free
-	os_hnd->free_os_handler(os_hnd);
+	while (1) {}
 }
